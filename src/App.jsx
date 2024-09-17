ae960c3e630c5cb79871dbc3f18eb13a8e56be0b
adnan <azizart320480@gmail.com>
2024-09-18 00:18:58 +0500

    change favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import Navbar from "./components/navbar/Navbar";
-import Card from "./components/musicCard/Card";
 
 function App() {
   return (
     <>
       <Navbar />
-      {/* <Card/> */}
     </>
   );
 }
